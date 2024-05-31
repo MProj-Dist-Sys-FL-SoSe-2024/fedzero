7eb4b9a0b4f0877f87909126e8e7b7fc1ad791f9
Leo Wilhelm Lierse <lierseleow@gmail.com>
2024-05-31 16:47:07 +0200

    Added comments

--- fedzero/config.py
+++ fedzero/config.py
@@ -4,7 +4,7 @@ NIID_DATA_SEED = 42  # controls how the data is split across clients
 SAVE_TRAINED_MODELS = False
 GUROBI_ENV = gurobipy.Env(params={"OutputFlag": 0})
 
-CRITICAL_LEARNING_OPTIMISATION = True
+CRITICAL_LEARNING_OPTIMISATION = False
 
 TIMESTEP_IN_MIN = 1  # minutes
 MAX_ROUND_IN_MIN = 60  # minutes
@@ -13,7 +13,7 @@ MAX_TIME_IN_DAYS = 7  # currently 11 max
 STOPPING_CRITERIA = None  # rounds without improved accuracy
 
 NUM_CLIENTS = 100
-CLIENTS_PER_ROUND = 3
+CLIENTS_PER_ROUND = 10
 BATCH_SIZE = 10
 MIN_LOCAL_EPOCHS = 1
 MAX_LOCAL_EPOCHS = 5
@@ -22,8 +22,8 @@ SOLAR_SIZE = 800  # W
 
 # Flower
 RAY_CLIENT_RESOURCES = {
-    "num_cpus": 3,  # CPU threads assigned to each client
-    "num_gpus": 1 / 3
+    "num_cpus": 1,  # CPU threads assigned to each client
+    # "num_gpus": 1 / 3
 }
 RAY_INIT_ARGS = {
     # "num_cpus": 8,  # Number of physically accessible CPUs
