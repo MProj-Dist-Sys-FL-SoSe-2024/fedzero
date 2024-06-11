b3e1038f7b911097ff325fd707992c21ec94044e
Christian Helbig <ch@christianhelbig.com>
2024-06-11 12:59:18 +0200

    Added comments for better understanding the utility judges

--- fedzero/utility.py
+++ fedzero/utility.py
@@ -54,37 +54,75 @@ class ParticipationJudge(UtilityJudge):
         return "part"
         
     def utility(self) -> Dict[Client, float]:
-        participation = self._calculate_participation()
-        min_participation = max(1, min(participation.values()))
+        """
+        Calculate and return dictionary of clients and their respective utility.
+        Utility for each client is calculated as:
+            (min_participation / past_participation) ** self.weighting_exponent
+
+        Returns:
+            Dict[Client, float]: Dictionary of clients and their respective utility.
+        """
+        participation = self._calculate_participation()             # dict of clients and their respective participation
+        min_participation = max(1, min(participation.values()))     # minimum participation among all clients or 1
         weighting = {}
+
+        # calculate the utility for each client
         for client, past_participation in participation.items():
             try:
+                # calculate the utility for the client
                 weighting[client] = (min_participation / past_participation) ** self.weighting_exponent
             except ZeroDivisionError:
+                # if the client has not participated in any round, set the utility to 1
                 weighting[client] = 1
         return weighting
 
     def _calculate_participation(self) -> Dict[Client, int]:
+        """
+        Calculate and return dictionary of clients and their respective participation.
+        Keys are clients and values are the number of rounds they have participated in.
+
+        Returns:
+            Dict[Client, int]: Dictionary of clients and their respective participation.
+        """
         participation_dict: Dict[Client, int] = defaultdict(int)
+        # iterate over all clients and sum up their participation
         for client in self.clients:
+            # add the number of rounds the client has participated in to the participation_dict
             participation_dict[client] += client.participated_rounds
         return participation_dict
 
 
 class StatUtilityJudge(UtilityJudge):
-    """Weights clients based on their statistical utility."""
+    """
+    Weights clients based on their statistical utility.
+
+    Statistical utility is a measure for the quality of the data of a client. It can be interpreted as the
+    overall importance of a client for the learning process.
+    Clients with a higher statistical utility will receive a higher weighting and are given more importance in the
+    learning process.
+
+    The statistical utility is normalized to return a weighting between 0 and 1 for each client.
+    It is calculated as the difference between the statistical utility of a client and the minimum statistical utility
+    divided by the range of statistical utilities.
+    """
 
     def __repr__(self):
         return "stat"
 
     def utility(self) -> Dict[Client, float]:
+        # create dictionary of clients and their respective statistical utility.
+        # statistical utility is obtained from the client's statistical_utility() method
         statistical_utility_dict = {client: client.statistical_utility() for client in self.clients}
+
+        # min and max statistical utility among all clients
         min_utility = min(statistical_utility_dict.values())
         max_utility = max(statistical_utility_dict.values())
 
         weighting = {}
+        # calculate the utility for each client
         for client, util in statistical_utility_dict.items():
             try:
+                # normalize the statistical utility to return a weighting between 0 and 1
                 weighting[client] = (statistical_utility_dict[client] - min_utility) / (max_utility - min_utility)
             except ZeroDivisionError:
                 weighting[client] = 1
