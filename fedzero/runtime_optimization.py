a22bd449877556d9bc0c9d850cd5eeb0e580e66f
Leo Wilhelm Lierse <lierseleow@gmail.com>
2024-08-19 12:44:08 +0200

    Add comments to runtime_optimization

--- fedzero/runtime_optimization.py
+++ fedzero/runtime_optimization.py
@@ -6,9 +6,7 @@ import gurobipy as grb
 import numpy as np
 import pandas as pd
 
-from fedzero.config import TIMESTEP_IN_MIN, MAX_ROUND_IN_MIN, MIN_LOCAL_EPOCHS, MAX_LOCAL_EPOCHS, \
-    CLIENTS_PER_ROUND, GUROBI_ENV, ENABLE_BROWN_CLIENTS, TIME_WINDOW_LOWER_BOUND, \
-    TIME_WINDOW_UPPER_BOUND
+from fedzero.config import TIMESTEP_IN_MIN, MAX_ROUND_IN_MIN, MIN_LOCAL_EPOCHS, MAX_LOCAL_EPOCHS, GUROBI_ENV
 from fedzero.entities import PowerDomainApi, ClientLoadApi, Client
 
 EPSILON = 0.0001
@@ -81,6 +79,7 @@ def _execute_power_domain_round(power_domain_api: PowerDomainApi,
             c: int(min(client_load_api.actual(now, c.name), c.batches_per_epoch * max_epochs - participation[c])) 
             for c in participation.keys() if (not c.is_brown)
         }
+        # For brown clients, always compute the expected batches
         max_batches.update(
             {
                 c: selection[now][c] for c in participation.keys() if c.is_brown
@@ -101,14 +100,13 @@ def _execute_power_domain_timestep(clients: List[Client],
     """Simulates the execution of a training round for one timestep within a power domain."""
     if len(clients) == 1:
         c = clients[0]
-        if c.is_brown:
+        if c.is_brown: 
             participation[c] += max_batches[c]
-        else:
+        else: # For brown clients, always compute the expected batches
             participation[c] += min(available_energy / c.energy_per_batch, max_batches[c])
     else:
         # First attribute energy to clients that haven't reached MIN_LOCAL_EPOCHS
-        participation1, remaining_energy = _attribute_power(MIN_LOCAL_EPOCHS, participation, available_energy,
-                                                            max_batches)
+        participation1, remaining_energy = _attribute_power(MIN_LOCAL_EPOCHS, participation, available_energy, max_batches)
         for c, p in participation1.items():
             participation[c] += p
             max_batches[c] -= p
@@ -126,23 +124,27 @@ def _attribute_power(required_epochs, participation, available_energy, max_batch
     """Attributes power to all clients below <required_epochs>."""
     missing_batches = {c: c.batches_per_epoch * required_epochs - p for c, p in participation.items()}
     clients: list[Client] = [c for c in participation.keys() if missing_batches[c] > 0]
+    # Weight clients (exclude brown clients)
     weighting = {c: missing_batches[c] * c.energy_per_batch for c in clients if (not c.is_brown)}
 
     model = grb.Model(name="Runtime power attribution model", env=GUROBI_ENV)
 
     # capping the available_energy to the max of possible usage allows us to use an equality constraint in (1)
+    # Do not cap brown clients
     _available_energy = min(available_energy, sum([max_batches[c] * c.energy_per_batch for c in clients if (not c.is_brown)]))
 
     m = {c: model.addVar(lb=0, ub=max_batches[c]) for c in clients}
     y = {c: model.addVar(vtype=grb.GRB.BINARY) for c in clients}
     x = model.addVar(lb=0, ub=_available_energy/EPSILON)
 
+    # Limit green client energy usage to green energy budget
     model.addConstr(grb.quicksum(m[c] * c.energy_per_batch for c in clients if (not c.is_brown)) <= _available_energy)
     for c in [_client for _client in clients if (not _client.is_brown)]:
         model.addGenConstrIndicator(y[c], False, m[c] == x * weighting[c])
         model.addGenConstrIndicator(y[c], True, m[c] >= max_batches[c])
         model.addGenConstrIndicator(y[c], True, x * weighting[c] >= max_batches[c])
 
+    # For brown clients, always compute the expected batches
     for c in [_client for _client in clients if _client.is_brown]:
         model.addConstr(y[c] == True)
         model.addConstr(m[c] == max_batches[c])
@@ -153,6 +155,7 @@ def _attribute_power(required_epochs, participation, available_energy, max_batch
 
     if model.Status == grb.GRB.OPTIMAL:
         participation = {c: xc.X for c, xc in m.items()}
+        # Calculate remaining energy, but ignore brown client consumption
         remaining_energy = available_energy - sum(p * c.energy_per_batch for c, p in participation.items() if (not c.is_brown))
         return participation, 0 if np.isclose(remaining_energy, 0) else remaining_energy
     elif model.Status == grb.GRB.INFEASIBLE:
